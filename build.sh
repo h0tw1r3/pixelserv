25089c8fa4dd52a026bc3efa1071d795bf7b6521
Jeffrey Clark <dude@zaplabs.com>
2014-01-14 11:52:04 -0600

    build script housekeeping

--- build.sh
+++ build.sh
@@ -16,15 +16,13 @@ ls -laF $BIN
 # use Linksys Tomato toolchain (or teddy_bear tomatousb K26, Tornado dd-wrt)
 export PATH=/opt/brcm/hndtools-mipsel-uclibc/bin:/opt/brcm/hndtools-mipsel-linux/bin:$PATH
 CC="mipsel-uclibc-gcc -mips32"
-CFLAGS="-Os -s -Wall -ffunction-sections -fdata-sections"
-LDFLAGS="-Wl,--gc-sections"
-STRIP="mipsel-uclibc-strip -s -R .note -R .comment -R .gnu.version -R .gnu.version_r"
+STRIP="mipsel-uclibc-$STRIP"
 
 #tomato
 # -DIF_MODE "-i br0" responsible for failures when gui changes made
 # -DVERBOSE"
 BIN=pixelserv
-$CC $CFLAGS $LDFLAGS $OPTS $SRC -o $BIN
+$CC $CFLAGS $LDFLAGS $OPTS $TEST_OPTS $SRC -o $BIN
 $STRIP $BIN
 ls -laF $BIN
 
